54b4c66e76f7b25bb18b0e4bf602b4d3305c7ed6
AEMuto <antoine.marseaud@gmail.com>
2021-11-05 16:38:28 +0100

    Ajout test unitaire composant container/Bills : faire passer le taux de couverture à 80% ✅

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -1,15 +1,15 @@
 import VerticalLayout from './VerticalLayout.js'
 import ErrorPage from "./ErrorPage.js"
 import LoadingPage from "./LoadingPage.js"
-
 import Actions from './Actions.js'
+import { formatDate } from '../app/format.js';
 
 const row = (bill) => {
   return (`
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
-      <td>${bill.date}</td>
+      <td>${formatDate(bill.date)}</td>
       <td>${bill.amount} €</td>
       <td>${bill.status}</td>
       <td>
