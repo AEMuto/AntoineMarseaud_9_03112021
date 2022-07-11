1dd84cc822169d23206fe99c6bac00f84beede9f
AEMuto <antoine.marseaud@gmail.com>
2022-07-11 19:08:26 +0200

    Making changes to serve the app on Heroku.

---
+++ server.js
@@ -0,0 +1,11 @@
+import liveServer from 'live-server';
+
+var params = {
+	port: 8181, // Set the server port. Defaults to 8080.
+	file: "./index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+	wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+	logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+	middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+};
+
+liveServer.start(params);
